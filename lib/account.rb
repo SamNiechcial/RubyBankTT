60108364a102843b5ca2301df1e4e049246bb57e
Sam Niechcial <samniechcial@gmail.com>
2018-05-22 16:15:24 +0100

    Initialize method refactored

--- lib/account.rb
+++ lib/account.rb
@@ -2,11 +2,11 @@ class Account
   DEFAULT_BALANCE = 0.0
 
   def initialize(balance = DEFAULT_BALANCE)
-    @balance = DEFAULT_BALANCE
+    @balance = balance
   end
 
   def balance
     @balance
   end
-
+    
 end
