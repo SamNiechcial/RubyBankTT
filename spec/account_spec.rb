868bddfeddf2b901b33a36e72c6f98143a466208
Sam Niechcial <samniechcial@gmail.com>
2018-05-22 16:12:59 +0100

    Second test written and failing

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,4 +7,9 @@ describe Account do
   it 'should create an account with a balance of zero by default' do
     expect(sams_account.balance).to eq 0
   end
+
+  it 'should create an account with an empty transactions array' do
+    expect(sams_account.transactions).to eq []
+  end
+  
 end
