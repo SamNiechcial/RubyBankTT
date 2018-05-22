992e9afc9ee9e114bea55efdeaef7ebcc0f05dd8
Sam Niechcial <samniechcial@gmail.com>
2018-05-22 16:03:56 +0100

    First test written for Account Class

---
+++ spec/account_spec.rb
@@ -0,0 +1,9 @@
+
+require 'account'
+
+describe Account do
+  subject(:sams_account) { described_class.new }
+
+  it 'should create an account with a balance of zero by default' do
+    expect(sams_account.balance).to eq 0
+  end
