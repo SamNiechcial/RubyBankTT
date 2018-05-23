21923bd793d7a6307c8d5caabac485ec267abdee
Sam Niechcial <samniechcial@gmail.com>
2018-05-23 12:42:09 +0100

    Transaction class & tests refactored to remove unnecessary number variable

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -3,9 +3,8 @@ require 'account'
 class Transaction
   attr_reader :balance, :transaction
 
-  def initialize(number, date, deposit, withdrawal, balance)
-    @transaction = { number: number,
-                     date: date,
+  def initialize(date, deposit, withdrawal, balance)
+    @transaction = { date: date,
                      deposit: deposit,
                      withdrawal: withdrawal,
                      balance: balance }
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -2,11 +2,7 @@ require 'account'
 require 'transaction'
 
 describe Transaction do
-  subject(:sams_transaction) { described_class.new(1, "23/05/2018", 500, nil, 500) }
-
-  it 'should instantiate with the correct number' do
-    expect(sams_transaction.transaction[:number]).to eq 1
-  end
+  subject(:sams_transaction) { described_class.new("23/05/2018", 500, nil, 500) }
 
   it 'should instantiate with the correct date' do
     expect(sams_transaction.transaction[:date]).to eq "23/05/2018"
@@ -22,5 +18,5 @@ describe Transaction do
 
   it 'should instantiate with the correct balance value' do
     expect(sams_transaction.transaction[:balance]).to eq 500
-  end 
+  end
 end
