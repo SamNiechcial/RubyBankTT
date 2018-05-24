98001ca71097a2cc2e93ea0c36adfac631e0a7c4
Sam Niechcial <samniechcial@gmail.com>
2018-05-24 12:08:10 +0100

    account.rb refactored to use dependency injection for Transaction class

--- lib/account.rb
+++ lib/account.rb
@@ -6,21 +6,20 @@ class Account
 
   DEFAULT_BALANCE = 0.0
 
-  def initialize(balance = DEFAULT_BALANCE)
+  def initialize(balance = DEFAULT_BALANCE, transaction_class = Transaction)
     @balance = balance
     @current_date = Time.now.strftime('%d/%m/%Y')
     @statement = []
+    @transaction_class = transaction_class
   end
 
   def deposit(deposit_amount)
     @balance += deposit_amount
-    transaction = Transaction.new(@current_date, deposit_amount, nil, @balance)
-    @statement << transaction
+    @statement << @transaction_class.new(@current_date, deposit_amount, nil, @balance)
   end
 
   def withdraw(withdrawal_amount)
     @balance -= withdrawal_amount
-    transaction = Transaction.new(@current_date, nil, withdrawal_amount, @balance)
-    @statement << transaction
+    @statement << @transaction_class.new(@current_date, nil, withdrawal_amount, @balance)
   end
 end
