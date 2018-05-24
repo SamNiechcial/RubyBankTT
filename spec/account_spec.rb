7a8f78702ecc92aec0e0fd6ac8838285e5ec5e31
Sam Niechcial <samniechcial@gmail.com>
2018-05-24 12:01:01 +0100

    account_spec.rb refactored to use subject and use change by methods for deposit and withdrawal

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,44 +5,42 @@ describe Account do
   subject(:sams_account) { described_class.new }
 
   it 'should create an account with a balance of zero by default' do
-    expect(sams_account.balance).to eq 0
+    expect(subject.balance).to eq 0
   end
 
   it 'should create an account with an empty statement array' do
-    expect(sams_account.statement).to eq []
+    expect(subject.statement).to eq []
   end
 
   describe '#deposit' do
     it 'should allow the user to deposit money in the account' do
-      sams_account.deposit(10)
-      expect(sams_account.balance).to eq 10.00
+      expect{subject.deposit(10)}.to change{subject.balance}.from(0).to(10.00)
     end
 
     it 'should add one transaction object to the statement on deposit' do
-      sams_account.deposit(100)
-      expect(sams_account.statement.length).to eq 1
+      subject.deposit(100)
+      expect(subject.statement.length).to eq 1
     end
 
     it 'should add a transaction to the statement containing correct amount' do
-      sams_account.deposit(100)
-      expect(sams_account.statement[0].transaction[:deposit]).to eq 100
+      subject.deposit(100)
+      expect(subject.statement[0].transaction[:deposit]).to eq 100
     end
   end
 
   describe '#withdraw' do
     it 'should allow the user to withdraw money from the account' do
-      sams_account.withdraw(10)
-      expect(sams_account.balance).to eq -10
+      expect{subject.withdraw(10)}.to change{subject.balance}.from(0).to(-10)
     end
 
     it 'should add one transaction object to the statement on withdrawal' do
-      sams_account.withdraw(100)
-      expect(sams_account.statement.length).to eq 1
+      subject.withdraw(100)
+      expect(subject.statement.length).to eq 1
     end
 
     it 'should add a transaction to the statement containing correct amount' do
-      sams_account.withdraw(100)
-      expect(sams_account.statement[0].transaction[:withdrawal]).to eq 100
+      subject.withdraw(100)
+      expect(subject.statement[0].transaction[:withdrawal]).to eq 100
     end
   end
 end
